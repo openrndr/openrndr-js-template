357afd58f56c785c409ca6f389c458f3c2855d87
Abe Pazos <abe@hamoid.com>
2021-07-07 10:53:08 +0200

    Make it easier to toggle orx'es in template (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,39 @@ plugins {
 }
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+/*  Which version of OPENRNDR and ORX should be used? */
+val openrndrUseSnapshot = true
+val openrndrVersion = if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "?.?.??"
+
+val orxUseSnapshot = true
+val orxVersion = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "?.?.??"
+
+/*  Which additional multiplatform (ORX) libraries should be added to this project. */
+val orxFeatures = setOf(
+//    "orx-camera",
+//    "orx-color",
+//    "orx-compositor",
+//    "orx-easing",
+    "orx-fx",
+//    "orx-gradient-descent",
+    "orx-image-fit",
+    "orx-noise",
+//    "orx-parameters",
+//    "orx-shade-styles",
+//    "orx-shader-phrases",
+//    "orx-quadtree",
+    null
+).filterNotNull()
+
+fun orx(module: String): Any {
+    return "org.openrndr.extra:$module:$orxVersion"
+}
+
+fun openrndr(module: String): Any {
+    return "org.openrndr:openrndr-$module:$openrndrVersion"
+}
+
 repositories {
     mavenCentral()
     mavenLocal()
@@ -10,12 +43,17 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-js"))
     implementation(kotlin("reflect"))
-    implementation("org.openrndr:openrndr-application:0.5.1-SNAPSHOT")
-    implementation("org.openrndr:openrndr-dds:0.5.1-SNAPSHOT")
-    implementation("org.openrndr:openrndr-draw:0.5.1-SNAPSHOT")
-    implementation("org.openrndr:openrndr-webgl:0.5.1-SNAPSHOT")
-    implementation("org.openrndr.extra:orx-fx:0.5.1-SNAPSHOT")
+
+    implementation(openrndr("application"))
+    implementation(openrndr("dds"))
+    implementation(openrndr("draw"))
+    implementation(openrndr("webgl"))
+
+    for (feature in orxFeatures) {
+        implementation(orx(feature))
+    }
 }
+
 kotlin {
     js(IR) {
         browser {
