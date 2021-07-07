5b1aeab000387a848466b735571f5753c052f428
Abe Pazos <abe@hamoid.com>
2021-07-07 09:50:24 +0200

    Make it easier to toggle orx'es in template

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,61 @@ plugins {
 }
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val orxUseSnapshot = true
+val orxVersion = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "?.?.??"
+
+/*  Which additional (ORX) libraries should be added to this project. */
+val orxFeatures = setOf(
+//    "orx-boofcv",
+//    "orx-camera",
+    "orx-color",
+//    "orx-compositor",
+    "orx-fx",
+//    "orx-glslify",
+//    "orx-gui",
+//    "orx-image-fit",
+//    "orx-integral-image",
+//    "orx-jumpflood",
+//    "orx-mesh-generators",
+//    "orx-midi",
+//    "orx-no-clear",
+//    "orx-noise",
+//    "orx-olive",
+//    "orx-osc",
+//    "orx-palette",
+//    "orx-panel",
+//    "orx-parameters",
+//    "orx-poisson-fill",
+//    "orx-shade-styles",
+//    "orx-shapes",
+//    "orx-triangulation",
+//    "orx-video-profiles",
+//    "poc-orx-keyframer",
+//    "orx-chataigne",
+//    "orx-dnk3",
+//    "orx-easing",
+//    "orx-file-watcher",
+//    "orx-filter-extension",
+//    "orx-gradient-descent",
+//    "orx-interval-tree",
+//    "orx-kdtree",
+//    "orx-kinect-v1",
+//    "orx-obj-loader",
+//    "orx-rabbit-control,
+//    "orx-runway",
+//    "orx-shader-phrases",
+//    "orx-syphon",
+//    "orx-temporal-blur",
+//    "orx-tensorflow",
+//    "orx-time-operators,
+    null
+).filterNotNull()
+
+fun orx(module: String): Any {
+    return "org.openrndr.extra:$module:$orxVersion"
+}
+
 repositories {
     mavenCentral()
     mavenLocal()
@@ -14,8 +69,13 @@ dependencies {
     implementation("org.openrndr:openrndr-dds:0.5.1-SNAPSHOT")
     implementation("org.openrndr:openrndr-draw:0.5.1-SNAPSHOT")
     implementation("org.openrndr:openrndr-webgl:0.5.1-SNAPSHOT")
-    implementation("org.openrndr.extra:orx-fx:0.5.1-SNAPSHOT")
+
+    for (feature in orxFeatures) {
+        implementation(orx(feature))
+    }
+
 }
+
 kotlin {
     js(IR) {
         browser {
