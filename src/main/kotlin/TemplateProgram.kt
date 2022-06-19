d19feb05bbcdc55e847fabc242efab52036f7656
vechro <vechro@vech.ro>
2022-06-19 17:55:58 +0300

    Rename to TemplateProgram to be more consistent with openrndr-template

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -4,15 +4,12 @@ import org.openrndr.color.rgb
 import kotlin.math.cos
 
 suspend fun main() = applicationAsync {
-    // workaround for kotlin/js ignoring the initializer because it's "unused"
-    println(org.openrndr.webgl.applicationWebGLInitializer)
-
     program {
         extend {
             val a = rgb("#ff0000")
             drawer.clear(a)
             drawer.fill = ColorRGBa.WHITE
-            drawer.circle(width/2.0, height/2.0, 100.0 + cos(seconds)*40.0)
+            drawer.circle(width / 2.0, height / 2.0, 100.0 + cos(seconds) * 40.0)
         }
     }
 }
