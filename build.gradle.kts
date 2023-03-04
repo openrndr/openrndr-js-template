a0b78a84822bb0c80c345baf6668674e417b7d64
Vechro <6316604+Vechro@users.noreply.github.com>
2023-03-04 16:09:42 +0200

    Upgrade dependencies and cleanup (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("js") version "1.7.21"
+    alias(libs.plugins.kotlin.js)
 }
+
 group = "org.example"
 version = "1.0-SNAPSHOT"
-
-/*  Which version of OPENRNDR and ORX should be used? */
-val openrndrUseSnapshot = false
-val openrndrVersion = if (openrndrUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.2-rc.2"
-
-val orxUseSnapshot = false
-val orxVersion = if (orxUseSnapshot) "0.5.1-SNAPSHOT" else "0.4.1-rc.2"
 
 /*  Which additional multiplatform (ORX) libraries should be added to this project. */
 val orxFeatures = setOf<String>(
@@ -28,26 +23,20 @@ val orxFeatures = setOf<String>(
 //    "orx-quadtree",
 )
 
-fun orx(module: String): Any {
-    return "org.openrndr.extra:$module:$orxVersion"
-}
-
-fun openrndr(module: String): Any {
-    return "org.openrndr:openrndr-$module:$openrndrVersion"
-}
+fun orx(module: String) = "org.openrndr.extra:$module:${libs.versions.orx.get()}"
 
 repositories {
     mavenCentral()
     mavenLocal()
 }
-dependencies {
-    implementation(kotlin("stdlib-js"))
-    implementation(kotlin("reflect"))
 
-    implementation(openrndr("application"))
-    implementation(openrndr("dds"))
-    implementation(openrndr("draw"))
-    implementation(openrndr("webgl"))
+dependencies {
+    implementation(libs.kotlin.reflect)
+
+    implementation(libs.openrndr.application)
+    implementation(libs.openrndr.dds)
+    implementation(libs.openrndr.draw)
+    implementation(libs.openrndr.webgl)
 
     for (feature in orxFeatures) {
         implementation(orx(feature))
@@ -57,17 +46,10 @@ dependencies {
 kotlin {
     js(IR) {
         browser {
-            webpackTask {
+            commonWebpackConfig {
                 outputFileName = "openrndr-program.js"
                 cssSupport {
-                    enabled = true
-                }
-
-            }
-            runTask {
-                outputFileName = "openrndr-program.js"
-                cssSupport {
-                    enabled = true
+                    enabled.set(true)
                 }
             }
         }
